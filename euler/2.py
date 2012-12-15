7d63424f006c5d2ea58d8801fd9b33807562bcf5
Alexander Harkness <bearbin@gmail.com>
2012-12-15 13:06:14 +0000

    Added PE problem 2

---
+++ euler/2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Copyright © 2012 Alexander Harkness
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+#and associated documentation files (the “Software”), to deal in the Software without restriction,
+#including without limitation the rights to use, copy, modify, merge, publish, distribute,
+#sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or
+#substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+#BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# This is puzzle #2 from projecteuler.net
+
+def gen_fibonacci():
+    z = 0
+    first = 1
+    second = 1
+    sequence = []
+    while z < 4000000:
+        z = first + second
+        sequence.append(z)
+        first = second
+        second = z
+    return sequence
+
+p = 0
+
+for i in gen_fibonacci():
+    if i % 2 is 0:
+        p += i
+
+print(p)
